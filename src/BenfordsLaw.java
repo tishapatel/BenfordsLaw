f8c39237c0d9811c230d3fae6aceaee5b2f79e54
Sasha-T800 <340951334@gapps.yrdsb.ca>
2021-05-12 11:25:38 -0400

    add output function result csv

--- src/BenfordsLaw.java
+++ src/BenfordsLaw.java
@@ -10,16 +10,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class BenfordsLaw {
 
     public static void main(String[] args) throws FileNotFoundException {
         // Read sales numbers from sales.csv in project
-        // Calculate frequencies which will be array where index is digit and value is frequency of that digit (%)
+        // Calculate frequencies which will be array where index is digit and value is
+        // frequency of that digit (%)
         ArrayList<String> sales = readSalesFromCsvFile();
         double[] frequencies = calculateFrequencies(sales);
 
-        // Numeric and visual representation of frequencies of digits from 1 - 9 from sales
+        // Numeric and visual representation of frequencies of digits from 1 - 9 from
+        // sales
         printNumericRepresentation(frequencies);
         createVisualRepresentation(frequencies);
 
@@ -31,13 +34,31 @@ public class BenfordsLaw {
         }
 
         // Output frequencies to csv
-        // outputToCsv(frequencies);
+        outputToCsv(frequencies);
+    }
+
+    private static void outputToCsv(double[] frequencies) throws FileNotFoundException {
+        File fileOut = new File("results.csv");
+
+        // java utils allows write to file
+        PrintWriter out = new PrintWriter(fileOut);
+        // fist line
+        String columnsName = "Digits,Frecency(%)\n";
+        out.write(columnsName);
+        for (int i = 1; i < frequencies.length; i++) {
+            // Set value of digit i (1 - 9) -> frequency percentage of i in csv file
+            String line = Integer.toString(i) + "," + Double.toString(frequencies[i]) + "\n";
+            out.write(line);
+        }
+
+        out.close();
+
     }
 
     /**
-      * @return ArrayList<String>
-      * @throws FileNotFoundException
-      */
+     * @return ArrayList<String>
+     * @throws FileNotFoundException
+     */
     public static ArrayList<String> readSalesFromCsvFile() throws FileNotFoundException {
         ArrayList<String> salesDataArray = new ArrayList<String>();
 
@@ -49,18 +70,16 @@ public class BenfordsLaw {
 
         // make scanner for read from sales file
         Scanner readerFile = new Scanner(salesFile);
-
+        readerFile.useDelimiter(",");
         // returns a boolean value
         while (readerFile.hasNext()) {
             String data = readerFile.next();
             // check if data is not empty
             if (!data.isEmpty()) {
-                // split data for 2 strings
-                String[] splitData = data.split(",");
-                // check if strings array is length 2 and second string is number
-                if (splitData.length == 2 && isNumeric(splitData[1])) {
-                    // create instance of Data and add to array
-                    salesDataArray.add(splitData[1]);
+                // check if string is number
+                if (Character.isDigit(data.charAt(0))) {
+                    // add to array
+                    salesDataArray.add(data);
                 }
             }
         }
@@ -71,9 +90,9 @@ public class BenfordsLaw {
     }
 
     /**
-      * @param str
-      * @return boolean check strin if it is numeric
-      */
+     * @param str
+     * @return boolean check strin if it is numeric
+     */
     public static boolean isNumeric(String str) {
         try {
             Long.parseLong(str);
@@ -84,13 +103,14 @@ public class BenfordsLaw {
     }
 
     /***
-      * Takes sales that were read in from .csv and will calculate the first digit frequencies of those sales
-      * as a percentage of all digits. The resulting double[] will be digit -> frequency.
-      * Example: frequencies[1] is the frequency of first digit being 1.
-      *
-      * @param sales that were read in from sales.csv
-      * @return double[] the frequencies calculated from sales
-      */
+     * Takes sales that were read in from .csv and will calculate the first digit
+     * frequencies of those sales as a percentage of all digits. The resulting
+     * double[] will be digit -> frequency. Example: frequencies[1] is the frequency
+     * of first digit being 1.
+     *
+     * @param sales that were read in from sales.csv
+     * @return double[] the frequencies calculated from sales
+     */
     public static double[] calculateFrequencies(ArrayList<String> sales) {
         double[] occurrences = new double[10];
         double[] frequencies = new double[10];
@@ -101,7 +121,8 @@ public class BenfordsLaw {
             occurrences[firstDigit] += 1;
         }
 
-        // Calculate the frequencies of each first digit (1 - 9) which is occurrences / total
+        // Calculate the frequencies of each first digit (1 - 9) which is occurrences /
+        // total
         for (int i = 1; i < occurrences.length; i++) {
             frequencies[i] = (occurrences[i] / sales.size()) * 100;
         }
@@ -110,10 +131,12 @@ public class BenfordsLaw {
     }
 
     /**
-      * Prints out the sales first digit frequency percentages in a numeric representation
-      * @param frequencies is the sales firist digit frequencies
-      * @return void
-      */
+     * Prints out the sales first digit frequency percentages in a numeric
+     * representation
+     * 
+     * @param frequencies is the sales firist digit frequencies
+     * @return void
+     */
     public static void printNumericRepresentation(double[] frequencies) {
         System.out.println("digit,frequency (%)");
         for (int i = 1; i < frequencies.length; i++) {
@@ -122,10 +145,12 @@ public class BenfordsLaw {
     }
 
     /**
-      * Creates a visual representation (bar chart) of the sales first digit frequency precentages
-      * @param frequencies is the sales firist digit frequencies
-      * @return void
-      */
+     * Creates a visual representation (bar chart) of the sales first digit
+     * frequency precentages
+     * 
+     * @param frequencies is the sales firist digit frequencies
+     * @return void
+     */
     public static void createVisualRepresentation(double[] frequencies) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         for (int i = 1; i < frequencies.length; i++) {
@@ -133,24 +158,17 @@ public class BenfordsLaw {
             dataset.setValue(frequencies[i], "Frequency", Integer.toString(i));
         }
 
-        // Names the bar graph according to the frequency, digits and name 
-        JFreeChart chart = ChartFactory.createBarChart(
-            "Benfords Law Distribution Leading Digit",
-            "Digits",
-            "Frequency",
-            dataset,
-            PlotOrientation.VERTICAL,
-            true,
-            true,
-            false
-        );
+        // Names the bar graph according to the frequency, digits and name
+        JFreeChart chart = ChartFactory.createBarChart("Benfords Law Distribution Leading Digit", "Digits", "Frequency",
+                dataset, PlotOrientation.VERTICAL, true, true, false);
 
         // Create chart panel with the bar graph
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         chartPanel.setBackground(Color.white);
 
-        // Create JFrame and add chart panel to it, closing it does not terminal program, then set it to be visible
+        // Create JFrame and add chart panel to it, closing it does not terminal
+        // program, then set it to be visible
         JFrame frame = new JFrame();
         frame.add(chartPanel);
         frame.pack();
@@ -160,9 +178,9 @@ public class BenfordsLaw {
     }
 
     /**
-      * @param frequencies is the sales firist digit frequencies
-      * @return boolean true if fraud likely did not occur or false if it did occur
-      */
+     * @param frequencies is the sales firist digit frequencies
+     * @return boolean true if fraud likely did not occur or false if it did occur
+     */
     public static boolean didNoFraudOccur(double[] frequencies) {
         // Fraud likely did not occur if first digit frequency is between 29 and 32
         double firstDigitFrequency = frequencies[1];
